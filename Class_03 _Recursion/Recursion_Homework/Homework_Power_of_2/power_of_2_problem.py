cb3eebe197c5b1f4e9c3f688f16c10c85ac3a184
zachheaton <zacharymartinheaton@gmail.com>
2022-01-09 16:21:34 -0700

    recursion

--- Class_03 _Recursion/Recursion_Homework/Homework_Power_of_2/power_of_2_problem.py
+++ Class_03 _Recursion/Recursion_Homework/Homework_Power_of_2/power_of_2_problem.py
@@ -1,4 +1,4 @@
-# Given an integer n, return True if it is a power of 2. 
+# Given an integer n, return True if it is a power of 2.
 # An integer n is a power of two, if there exists an integer x such that n == 2^x
 
 # Examples:
@@ -13,3 +13,25 @@
 
 # Input : n = 13
 # Output : False
+
+# example input/output
+# 0 -- 1
+# 1 -- 2
+# 2 -- 4
+# 3 -- 8
+# 4 -- 16
+# 5 -- 32
+
+def power(n):
+    if n == 1 or n == 2:
+        print('True')
+        return True
+    elif n < 2:
+        print("false")
+        return False
+    else:
+        return power(n / 2)
+
+
+n = int(input("Input a number: "))
+power(n)
