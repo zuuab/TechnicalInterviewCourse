e326cbfc4dbfe8ced6eb7ae135bd0a384b8e61a7
zachheaton <zacharymartinheaton@gmail.com>
2022-01-12 17:19:58 -0700

    my stuff

--- Class_04_Array_Problems_Easy/Practice_Easy/Problems/best_time_to_buy_stock_problem.py
+++ Class_04_Array_Problems_Easy/Practice_Easy/Problems/best_time_to_buy_stock_problem.py
@@ -1,7 +1,37 @@
 # Say you have an array for which the ith element is the price of a given stock on day i
-# If you were only permitted to complete at most one transaction (i.e, buy one and 
+# If you were only permitted to complete at most one transaction (i.e, buy one and
 # sell one share of the stock) design an algorithm to find the maximum profit
 # This problem came from leetcode.com
 
 input_array = [7, 1, 5, 3, 6, 4]
 # Output = 5
+# i = price
+
+
+# initial solution, double for loop
+def MaxProfit(val):
+    max = 0
+    for i in range(0, len(val)):
+        for j in range((i + 1), len(val)):
+            if (val[j] - val[i]) > max:
+                max = val[j] - val[i]
+    return max
+
+
+print(MaxProfit(input_array))
+
+
+# optimized solution, single for loop
+def MaxProfitOptimized(val):
+    max = 0
+    min = val[0]
+
+    for i in range(0, len(val)):
+        if val[i] < min:
+            min = val[i]
+        if (val[i] - min) > max:
+            max = (val[i] - min)
+    return max
+
+
+print(MaxProfitOptimized(input_array))
--- Class_04_Array_Problems_Easy/Practice_Easy/Problems/contains_duplicates_problem.py
+++ Class_04_Array_Problems_Easy/Practice_Easy/Problems/contains_duplicates_problem.py
@@ -2,5 +2,33 @@
 # Your function should return false if every element is distinct.
 # This problem came from leetcode.com
 
+from typing import Any
+
+
 input_array = [1, 2, 3, 3, 4]
 # Output = True
+
+
+# initial solution, double for loop
+def AnyRepeats(val):
+    for i in range(0, len(val)):
+        for j in range((i+1), len(val)):
+            if val[i] == val[j]:
+                return True
+    return False
+
+
+print(AnyRepeats(input_array))
+
+
+# optimized solution, single for loop
+def AnyRepeatsOptimized(val):
+    testSet = []
+    for i in range(0, len(val)):
+        if val[i] in testSet:
+            return True
+        testSet.append(val[i])
+    return False
+
+
+print(AnyRepeatsOptimized(input_array))
