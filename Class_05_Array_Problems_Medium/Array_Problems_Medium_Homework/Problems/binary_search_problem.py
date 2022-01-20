682c4721002314a9b6dae978b83cbf471e8b4233
zachheaton <zacharymartinheaton@gmail.com>
2022-01-20 07:43:47 -0700

    class work

--- Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
+++ Class_05_Array_Problems_Medium/Array_Problems_Medium_Homework/Problems/binary_search_problem.py
@@ -2,6 +2,9 @@
 # The function should return the index of the target number if the target number is found
 # or a -1 if the target is not found in the array.
 
+from tkinter.font import families
+
+
 def BinarySearch(array, n):
     l = 0
     u = len(array)-1
@@ -14,6 +17,7 @@ def BinarySearch(array, n):
             l = mid + 1
         else:
             u = mid - 1
+    return -1
 
 
 input_array = [1, 2, 5, 9, 12, 15, 21, 28, 99, 100, 117]
@@ -21,3 +25,58 @@ input_target = 5
 # Output = 2
 
 print(BinarySearch(input_array, input_target))
+
+
+def Search(array, n):
+    low = 0
+    high = len(array)-1
+    while low <= high:
+        mid = (high+low) // 2
+        if array[mid] == n:
+            return mid
+        elif array[mid] < n:
+            low = mid + 1
+        else:
+            high = mid - 1
+    return -1
+
+
+input_array = [1, 2, 5, 9, 12, 15, 21, 28, 99, 100, 117]
+input_target = 7
+# Output = 2
+
+
+print(Search(input_array, input_target))
+
+
+def OtherType(array):
+    low = 0
+    high = len(array)-1
+
+    while low < high:
+        mid = (high+low) // 2
+        if array[mid] > array[high]:
+            low = mid + 1
+        else:
+            high = mid
+    return array[low]
+
+
+new_array = [3, 4, 5, 6, 1, 2]
+
+print(OtherType(new_array))
+
+
+def MaxPivot(array):
+    low = 0
+    high = len(array) - 1
+    while low < high:
+        mid = (high+low) // 2
+        if array[mid] < array[low]:
+            high = mid - 1
+        else:
+            low = mid
+    return array[high]
+
+
+print(MaxPivot(new_array))
--- Class_05_Array_Problems_Medium/Practice_Medium/Problems/product_of_arrays_except_self_problem.py
+++ Class_05_Array_Problems_Medium/Practice_Medium/Problems/product_of_arrays_except_self_problem.py
@@ -3,5 +3,18 @@
 # Note: Please solve it without division
 # This problem came from leetcode.com
 
+def LetsDoThis(array):
+    new_array = []
+    for i in range(0, len(array)):
+        tempVal = 1
+        for j in range(0, len(array)):
+            if j != i:
+                tempVal = array[j] * tempVal
+        new_array.append(tempVal)
+    return new_array
+
+
 input_array = [1, 2, 3, 4]
 # Output = [24, 12, 8, 6]
+
+print(LetsDoThis(input_array))
